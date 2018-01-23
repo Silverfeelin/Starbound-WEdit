efe6778a85dad1b362588d3690e735eeb9d53e32
Silverfeelin <Silverfeelin@gmail.com>
2018-01-23 07:13:50 +0100

    weditTech check for previous functions

--- tech/dash/weditTech.lua
+++ tech/dash/weditTech.lua
@@ -3,20 +3,20 @@ require "/scripts/wedit/controller.lua"
 -- Hook init to initialize wedit.
 local ini = init
 function init()
-  ini()
+  if ini then ini() end
   wedit.controller.init()
 end
 
 -- Hook update to update wedit.
 local upd = update
 update = function(...)
-  upd(...)
+  if upd then upd(...) end
   wedit.controller.update(...)
 end
 
 -- Hook uninit to uninitialize wedit.
 local uni = uninit
 uninit = function()
-  uni()
+  if uni then uni() end
   wedit.controller.uninit()
 end
